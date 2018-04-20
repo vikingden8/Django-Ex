322caba01b44ca519c38e8b7ffba9b348814b389
Viking Den <vikingden7@gmail.com>
2018-04-21 00:01:24 +0800

    add exercise 05

--- article/admin.py
+++ article/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Article
 
 # Register your models here.
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'title', 'created_time', 'last_updated_time')
+    ordering = ('id',)
+
+
+admin.site.register(Article, ArticleAdmin)
--- article/models.py
+++ article/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_updated_time = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=1)
 
     def __str__(self):
         return self.title
