3bd797aa898008ae6e0a5564aaf1362b93fbed9e
Viking Den <vikingden7@gmail.com>
2018-04-24 10:41:47 +0800

    migrate database from sqlite3 to mysql

--- article/models.py
+++ article/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=120)
     content = models.TextField()
     created_time = models.DateTimeField(auto_now_add=True)
     last_updated_time = models.DateTimeField(auto_now=True)
